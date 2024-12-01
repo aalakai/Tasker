0df9f9d8cfa19f24d308f923dae41599747fe4e5
Drew Kringel <drewkringel@berkeley.edu>
2024-12-01 00:30:42 -0800

    Initial Commit

---
+++ Tasker/TaskerApp.swift
@@ -0,0 +1,32 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by Drew Kringel on 12/1/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TaskerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TaskerTests/TaskerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerTests.swift
+//  TaskerTests
+//
+//  Created by Drew Kringel on 12/1/24.
+//
+
+import Testing
+@testable import Tasker
+
+struct TaskerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
